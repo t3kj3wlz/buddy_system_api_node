2b94876609addf2778d50d9e001900896ace10c5
t3kj3wlz <t3kj3wlz4761@gmail.com>
2018-08-09 16:11:57 -0500

    Add files via upload

--- src/order.js
+++ src/order.js
@@ -1,6 +1,8 @@
 "use strict";
 
 const Record = require('./record');
+const Stash = require('./stash');
+const Inventory = require('./inventory');
 
 class BtcOrder extends Record{
 
@@ -32,6 +34,53 @@ class BtcOrder extends Record{
             var order = new BtcOrder();
             order.db.table(order.table).select('*').where("user = '" + username + "'").execute().then((data)=>{
                 resolve(data);
+            },(err)=>{
+                reject(err);
+            });
+        });
+    }
+    static initiate(vendorId,strainId,productAmount,btcAmount,usd_amount,btc_fees,btc_shipping,usd_shipping,usd_total_amount,username){
+        return new Promise((resolve,reject)=>{
+                var order = new BtcOrder();
+                order.vendor = vendorId;
+                order.strain = strainId;
+                order.product_amount = productAmount;
+                order.btc_amount = btcAmount;
+                order.usd_amount = usd_amount;
+                order.btc_fees = btc_fees;
+                order.shipping_amount_btc = btc_shipping;
+                order.shipping_amount_usd = usd_shipping;
+                order.usd_total_amount = usd_total_amount;
+                order.btc_total_amount = order.btc_amount + order.shipping_amount_btc + order.btc_fees;
+                order.user = username;
+                order._create().then((orderObj)=>{
+                    Stash.Stash.increase('btc',-1 * Math.abs(order.btc_total_amount)).then((stashObj)=>{
+                        resolve(orderObj);
+                    },(err)=>{
+                        reject(err);
+                    });
+                },(err)=>{
+                    reject(err);
+                });
+        });
+    }
+    static complete(orderId){
+        return new Promise((resolve,reject)=>{
+            var order = new Order(orderId);
+            order._build().then((orderData)=>{
+                if(order.received_date !== null){
+                    reject('Cannot Double Arrive Order.');
+                }
+                order.received_date = order.db.date();
+                order._update().then((orderObj)=>{
+                    Inventory.increase(order.strain,order.product_amount).then((inventoryObj)=>{
+                        resolve(order);
+                    },(err)=>{
+                        reject(err);
+                    });
+                },(err)=>{
+                    reject(err);
+                });
             },(err)=>{
                 reject(err);
             });
--- src/transfer.js
+++ src/transfer.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const Record = require('./record');
+const Stash = require('./stash');
 
 class BtcXfer extends Record{
 
@@ -28,6 +29,45 @@ class BtcXfer extends Record{
             var xfer = new BtcXfer();
             xfer.db.table(xfer.table).select('*').where("user = '" + username + "'").execute().then((data)=>{
                 resolve(data);
+            },(err)=>{
+                reject(err);
+            });
+        });
+    }
+    static initiate(stashUsed,initialRate,username){
+        return new Promise((resolve,reject)=>{
+            Stash.Stash.increase('usd',-1 * Math.abs(stashUsed),username).then((stashObj)=>{
+                var xfer = new BtcXfer();
+                xfer.stash_used = stashUsed;
+                xfer.initial_rate = initialRate;
+                xfer.user = username;
+                xfer._create().then((xferObj)=>{
+                    resolve(xferObj._buildPublicObj());
+                },(err)=>{
+                    reject(err);
+                });
+            },(err)=>{
+                reject(err);
+            });
+        });
+    }
+    static complete(UID,btcGained,completionRate){
+        return new Promise((resolve,reject)=>{
+            var xfer = new BtcXfer(UID);
+            xfer._build().then((xferData)=>{
+                xfer.btc_gained = btcGained;
+                xfer.complete = 1;
+                xfer.completion_rate = completionRate;
+                xfer.completion_date = xfer.db.date();
+                xfer._update().then((xferObj)=>{
+                    Stash.Stash.increase('btc',btcGained).then((stashObj)=>{
+                        resolve(xferObj._buildPublicObj());
+                    },(err)=>{
+                        reject(err);
+                    });
+                },(err)=>{
+                    reject(err);
+                });
             },(err)=>{
                 reject(err);
             });
