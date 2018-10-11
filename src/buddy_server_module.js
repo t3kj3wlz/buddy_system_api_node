c7e77553c1ddb52743d50654540162072b8c689c
t3kj3wlz <t3kj3wlz4761@gmail.com>
2018-10-11 10:06:25 -0500

    Update buddy_server_module.js

--- src/buddy_server_module.js
+++ src/buddy_server_module.js
@@ -500,7 +500,7 @@ var buddyServer = (function(){
         makePurchase:function(req,res,next){
             try{
                 _verifyToken(req.headers.auth_token).then((user)=>{
-                    Transaction.makePurchase(req.body.vendor,req.body.strain,req.body.product_amount,req.body.payment,user.username,req.body.front).then((transactionObj)=>{
+                    Transaction.makePurchase(req.body.buyer,req.body.strain,req.body.product_amount,req.body.payment,user.username,req.body.front).then((transactionObj)=>{
                         res.send(transactionObj._buildPublicObj());
                     },(err)=>{
                         res.send(err);
