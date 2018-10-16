0ee00b2e8177cb976fe603ca63b96e765e76cb59
t3kj3wlz <t3kj3wlz4761@gmail.com>
2018-10-16 11:34:41 -0500

    Update index.js

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, auth_token, request_token, password");
+  res.header("Access-Control-Allow-Methods", "*");
   next();
 });
 
